8e440a7c5b8a3c93ec5e9a2d33c3c082438a880d
Neeraj Singh <nrjsingh1@yahoo.in>
2019-06-16 17:31:58 +0530

    angular talknig to spring boot

---
+++ Bike-UI/src/app/services/bike.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs'
+const httpOptions= {
+  headers: new HttpHeaders({'Content-Type':'application-json'})
+};
+@Injectable({
+  providedIn: 'root'
+})
+export class BikeService {
+
+  constructor(private http: HttpClient) { }
+
+  getBikes() {
+    return this.http.get('/server/api/ver1/bike');
+  }
+}
