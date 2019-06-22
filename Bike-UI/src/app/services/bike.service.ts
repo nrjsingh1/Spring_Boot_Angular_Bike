b8f02bac2357df4dcd4988acc19bf24af4b56d1f
Neeraj Singh <nrjsingh1@yahoo.in>
2019-06-23 01:50:57 +0530

    Signed-off-by: Neeraj Singh <nrjsingh1@yahoo.in>

--- Bike-UI/src/app/services/bike.service.ts
+++ Bike-UI/src/app/services/bike.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs'
-const httpOptions= {
-  headers: new HttpHeaders({'Content-Type':'application-json'})
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application-json' })
 };
 @Injectable({
   providedIn: 'root'
@@ -12,6 +12,15 @@ export class BikeService {
   constructor(private http: HttpClient) { }
 
   getBikes() {
-    return this.http.get('/server/api/ver1/bike');
+    return this.http.get('/server/api/ver1/bikes');
+  }
+
+  getBike(id: Number) {
+    return this.http.get('/server/api/ver1/bikes/' + id);
+  }
+
+  createBikeRegistration(bike) {
+    let body = JSON.stringify(bike);
+    return this.http.post('server/api/ver1/bikes', body, httpOptions);
   }
 }
